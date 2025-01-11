823a707afbeed3ead42e5354c6e796f879efc48d
yagops0 <yagosalvador34@gmail.com>
2025-01-10 22:53:10 -0300

    Tentativo de usar Generics

---
+++ src/Generics/Box.java
@@ -0,0 +1,13 @@
+package Generics;
+
+public class Box<T>{
+    private Object object;
+
+    public Object getObject() {
+        return object;
+    }
+
+    public void setObject(Object object) {
+        this.object = object;
+    }
+}
--- src/interfaces/CrudContato.java
+++ src/interfaces/CrudContato.java
@@ -1,15 +1,16 @@
 package interfaces;
 
+import Generics.Box;
 import classes.Contato;
 
 import java.util.List;
 
-public interface CrudContato {
-    public int retornarIndice(Contato c);
-    public boolean existeContato(String nome);
-    public boolean create(Contato contato);
-    public Contato read(String nome);
-    public boolean update(Contato contato);
-    public boolean delete(Contato contato);
-    public List<Contato> listAll();
+public interface CrudContato{
+    public int retornarIndice(Box c);
+    public boolean existeContato(Box<> nome);
+    public boolean create(Box<> contato);
+    public Box<> read(String nome);
+    public boolean update(Box<> contato);
+    public boolean delete(Box<> contato);
+    public List<> listAll();
 }
